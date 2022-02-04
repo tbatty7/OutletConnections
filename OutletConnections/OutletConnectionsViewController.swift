f2b3bf63f2b68d8fa2bfdfbcf57f90b079588beb
Tim Batty <tbatty7@gmail.com>
2022-02-04 14:15:26 -0500

    Added button and label and wired them up

---
+++ OutletConnections/OutletConnectionsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  OutletConnectionsViewController.swift
+//  OutletConnections
+//
+//  Created by Timothy D Batty on 2/4/22.
+//
+
+import UIKit
+
+class OutletConnectionsViewController: UIViewController {
+
+    @IBOutlet private var label: UILabel!
+    @IBOutlet private var button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
