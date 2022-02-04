6df4b75f254e17c1f29b92164bba1abe97e76f11
Tim Batty <tbatty7@gmail.com>
2022-02-04 13:17:19 -0500

    Initial Commit

---
+++ OutletConnections/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OutletConnections
+//
+//  Created by Timothy D Batty on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
